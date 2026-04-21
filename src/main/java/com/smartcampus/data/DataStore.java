f8dbda8b986ec143958ab12f79d64390993ca857
Yasith Perera <yasithperera294@gmail.com>
2026-04-21 06:27:36 +0530

    feat: add DataStore

---
+++ src/main/java/com/smartcampus/data/DataStore.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.smartcampus.data;
+
+import com.smartcampus.model.Room;
+import com.smartcampus.model.Sensor;
+import com.smartcampus.model.SensorReading;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ * @author YasithMP
+ */
+public class DataStore {
+
+    private static DataStore instance;
+
+    private final Map<String, Room> rooms = new ConcurrentHashMap<>();
+    private final Map<String, Sensor> sensors = new ConcurrentHashMap<>();
+    private final Map<String, SensorReading> readings = new ConcurrentHashMap<>();
+
+    private DataStore() {
+        
+    }
+
+    public static synchronized DataStore getInstance() {
+        if (instance == null) {
+            instance = new DataStore();
+        }
+        return instance;
+    }
+
+    public Map<String, Room> getRooms() {
+        return rooms;
+    }
+
+    public Map<String, Sensor> getSensors() {
+        return sensors;
+    }
+
+    public Map<String, SensorReading> getReadings() {
+        return readings;
+    }
+    
+}
